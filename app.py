7f8f40ce36af59205b10949d457b911c432db135
Abhiram A <143158315+abcyclonz@users.noreply.github.com>
2024-09-09 00:09:06 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import cv2
+import pandas as pd
+import numpy as np
+from ultralytics import YOLO
+import time
+
+model=YOLO('yolov8s.pt')
+
+
+def RGB(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE :  
+        colorsBGR = [x, y]
+        print(colorsBGR)
+        
+
+cv2.namedWindow('RGB')
+cv2.setMouseCallback('RGB', RGB)
